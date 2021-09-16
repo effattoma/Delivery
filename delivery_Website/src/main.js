34fc8aa12da642c0585b43559a9a572bbbe156e0
effattoma <effat35-2146@diu.edu.bd>
2021-09-16 13:43:16 +0600

    footer

--- delivery_Website/src/main.js
+++ delivery_Website/src/main.js
@@ -15,7 +15,7 @@ let nCount = selector => {
 
 let a = 0;
 $(window).scroll(function () {
-  // The .offset() method allows us to retrieve the current position of an element  relative to the document
+
   let oTop = $(".numbers").offset().top - window.innerHeight;
   if (a == 0 && $(window).scrollTop() >= oTop) {
     a++;
